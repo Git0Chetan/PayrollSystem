9187abed0e74e7724e1986967bd8b2c9f2b57b47
Chetan Patil <chetan249581@gmail.com>
2025-09-19 18:25:33 +0530

    added conditional code

---
+++ src/main/java/org/example/ConditionalExample.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ConditionalExample {
+    public static void main(String[] args) {
+        int score = 75;
+
+        if (score >= 60) {
+            System.out.println("You passed!");
+        } else {
+            System.out.println("You failed.");
+        }
+    }
+}
