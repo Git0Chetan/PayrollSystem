8a5e8d555a80de8d404d701b209aad2d96e3fcb2
Chetan Patil <chetan249581@gmail.com>
2025-09-24 21:35:40 +0530

    modified the data and report

--- src/main/java/org/example/Brain.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Brain {
-    public String sayHello(String name) {
-        if (name == null) return "Hello, Brain Autoamtion!";
-        return "Hello, " + name + "!";
-    }
-}
